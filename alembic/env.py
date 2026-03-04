72cb81a519cbb275ac5b183f7700a5d450b51be8
Micheal-Onyinye <michealphilominaprecious@gmail.com>
2026-03-04 17:43:55 +0100

    Configure Alembic and generate initial database migrations

--- alembic/env.py
+++ alembic/env.py
@@ -1,7 +1,7 @@
 import sys
 import os
 from logging.config import fileConfig
-
+from alembic import context
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
@@ -20,7 +20,7 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
     
-config.set_main_option("sqlalchemy.url", settings.database_url)
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL.replace("%", "%%"))
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
